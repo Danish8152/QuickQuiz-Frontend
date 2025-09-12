ceaa49cc8b92e676b5f8c0e0d9489f4ec1bd7567
Danish <mohddanish8152@gmail.com>
2025-09-12 17:18:26 +0530

    changes

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,16 +3,17 @@ import logo from "../assets/QuickQuiz-Logo.png";
 function Nav() {
     return (
         <>
-            <div>
+            <div className='Header flex'>
                 <img src={logo} alt="QuickQuiq-logo" className='logo'/>
-                <ul>
+                <ul className='nav-list flex'>
                     <li><a href="#">Home</a></li>
                     <li><a href="#">Categories</a></li>
                     <li><a href="#">Challenge</a></li>
                     <li><a href="#">About Us</a></li>
                 </ul>
-                <div className='Sign-in-up'>
-
+                <div className='sign-in-up'>
+                    <button className='sign-in'>Sign In</button>
+                    <button className='sign-up'>Sign Up</button>
                 </div>
             </div>
         </>
