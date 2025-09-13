908d61d40bb8cf24f5ff5b28d25d14d872021395
Danish <mohddanish8152@gmail.com>
2025-09-13 21:20:36 +0530

    changes in hero section

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -3,10 +3,10 @@ import "../assets/hero.css"
 
 function Hero() {
     return (
-        <section className="bg-black flex items-center justify-between">
+        <section className="bg-black flex items-center justify-between px-20">
             <div className="hero-info">
                 <h1 className="">Challenge your mind, track your progress, and level up your knowledge.</h1>
-                <div className="sub-hero"><h6>Why Choose Us?</h6>
+                <div className="sub-hero"><h6 className="">Why Choose Us?</h6>
                     <p>Interactive Learning – Engaging quizzes designed to make learning fun and effective.</p>
                     <p>Track Your Progress – Smart analytics to monitor your growth and improve over time.</p>
                     <p>Learn Anytime, Anywhere – Access quizzes across multiple subjects from any device.</p>
