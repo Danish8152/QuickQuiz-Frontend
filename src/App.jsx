a0c08cd3742ea7b6a8525ac3718ee2fcff18e1b0
Danish <mohddanish8152@gmail.com>
2025-09-17 23:37:40 +0530

    repleced tailwind with bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import './App.css'
 import Navbar from './Components/Navbar'
 import Hero from './Components/Hero'
 import Categories from './Components/Categories'
+import Challenge from './Components/Challenges'
+
 function App() {
   return (
     <>
       <Navbar/>
       <Hero/>
       <Categories/>
+      <Challenge/>
     </>
   )
 }
---
+++ src/Components/Challenges.jsx
@@ -0,0 +1,12 @@
+import "../assets/Challenges.css"
+
+function Challenge() {
+  return(
+    <>
+        <h1>hello</h1>
+    </>
+  )
+}
+
+export default Challenge;
+
