69c7bf89405dc4afd0c560b754068131237f6194
Danish <mohddanish8152@gmail.com>
2025-09-19 17:28:06 +0530

    testing

--- api/SignIn.js
+++ api/SignIn.js
@@ -1,5 +1,5 @@
-import { connectToDB } from "../../lib/mongodb";
-import User from "../../models/User.js";
+import { connectToDB } from "../lib/mongodb";
+import User from "../models/User.js";
 import bcrypt from "bcryptjs";
 
 export default async function handler(req, res) {
