937cec7dd21ad19b08ac95ba4d8a37779ae8ba2a
Danish <mohddanish8152@gmail.com>
2025-09-23 19:53:22 +0530

    changing

--- src/Components/ProfileSlider.jsx
+++ src/Components/ProfileSlider.jsx
@@ -1,7 +1,7 @@
 import '../assets/Navbar.css'
 function ProfileSidebar({ user, onClose }) {
   return (
-    <div className="fixed inset-0 bg-black/50 z-50 flex justify-end mt-5" >
+    <div className="fixed inset-0 bg-black/50 z-50 flex justify-end mt-5 outerProfile" >
       <div className="bg-black w-80 h-full p-6 shadow-lg transform transition-transform duration-300 mt-4 profileSlider">
         <h2 className="text-2xl font-bold mb-4 text-center">Profile</h2>
         {user ? (
