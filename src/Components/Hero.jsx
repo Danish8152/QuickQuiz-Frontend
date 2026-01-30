4a7a107bdef04359469518929478016ab1caafbc
Danish <mohddanish8152@gmail.com>
2026-01-31 00:30:34 +0530

    update hero

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,20 +1,32 @@
 import heroImg from "../assets/hero.png";
 import "../Style/hero.css"
+import yellowTick from "../assets/icons8-tick-48-yellow.png"
+import RedTick from "../assets/icons8-tick-48-redTick.png"
+import greenTick from "../assets/icons8-tick-48-greenTick.png"
 
 function Hero() {
     return (
         <section className="bg-black flex items-center justify-between px-20 heroOuter">
             <div className="hero-info">
-                <h1 className="">Challenge your mind, track your progress, and level up your knowledge.</h1>
-                <div className="sub-hero"><h6 className="">Why Choose Us?</h6>
-                    <p>Interactive Learning – Engaging quizzes designed to make learning fun and effective.</p>
-                    <p>Track Your Progress – Smart analytics to monitor your growth and improve over time.</p>
-                    <p>Learn Anytime, Anywhere – Access quizzes across multiple subjects from any device.</p>
-                    <div className="hero-cta"><button className="hero-cta-btn">Start Quiz Now</button></div>
+                <h1 className="">Test your knowledge with smart quizzes across multiple subjects.</h1>
+                <div className="sub-hero">
+                    <h6 className=""> QuickQuiz help student and learners practice and track knowledge with interactive, timed in detailed performance insights.</h6>
+                    <li className="hero-li"><img src={greenTick} alt="Green Tick" className="tick" />Interactive Quizzes </li>
+                    <li className="hero-li"><img src={yellowTick} alt="Yellow Tick" className="tick" /> Smart Analytics </li>
+                    <li className="hero-li"><img src={RedTick} alt="Red Tick" className="tick" /> Learn Anywhere </li>
+                    <div className="hero-cta">
+                        <button className="hero-cta-btn">Start Quiz Now</button>
+                        <button className="cta-btn">Browse Categories</button>
+                    </div>
                 </div>
+                <ul className="flex gap-3 pt-3">
+                    <li>🌟 10,000+ Quizzes played </li>
+                    <li>🎓 Used by students & learner</li>
+                    <li>📊 Intractive learner</li>
+                </ul>
             </div>
             <div className="hero-image">
-                <img src={heroImg} alt="Hero Img" />
+                <img src={heroImg} alt="Hero Img" style={{width: "900px", height: "700px"}}/>
             </div>
         </section>
     );
